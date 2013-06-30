9fa6b7e59686caab1104682630e8692990afa9fe
Allen Wu <allenwu1018@gmail.com>
2013-06-30 13:47:18 +0800

    section 9: i18n: translation to Spanish

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -4,6 +4,7 @@ class LineItem < ActiveRecord::Base
   belongs_to :cart
   attr_accessible :cart_id, :product_id, :quantity
   attr_accessible :product
+  attr_accessible :cart
 
   def total_price
     product.price * quantity
